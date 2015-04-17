b527b6f7308f646ddcf3e04a3c3d1994a1994788
Dalton <dalton.cco1@gmail.com>
2015-04-17 17:32:52 -0300

    Bugo tudo.... nao tinha mais para aonde atirar.... Feito alterações em algumas logicas.

--- Interpretador.java
+++ Interpretador.java
@@ -4,10 +4,12 @@ class Interpretador {
 	private String[] linhas;
     private Sintaxe sinta = new Sintaxe();
     private String[] tok;
+    private String[] aux;
+    private String a;
    
     
     public void interpreta(String l[]) {
-		
+		int rec = 0;
 		for(int i = 0; i < l.length; i++){
 			if(l[i] != null && l[i].equals(" ")){
 				linhas = l[i].split(" ");
@@ -19,29 +21,81 @@ class Interpretador {
 			}
 		}
 		
+		for(int j = 0; j < l.length && l[j] != null; j++){
+			if(l[j].length() > 1){
+				rec++;
+			}
+		}
+		//System.out.println(rec);
+		for(int cont = 0; cont <= l.length && l[cont] != null; cont++){
+			if(l[cont].length() > 1){
+				tok = l[cont].trim().split(" ");
+				a = tok[0];
+				switch(a){
+				
+					case "se":
+						for(int i = 0; i < l.length && l[i] != null; i++){
+							aux = l[i].trim().split(" ");
+							if(aux.length > 1 && aux[0].equals("fim") && aux[1].equals("se")){
+								System.out.println("deu certo o se");
+								if(sinta.se(tok)){
+									break;
+								}else{
+									cont = i;
+									System.out.println(cont);
+									System.out.println(rec);
+									break;
+								}				
+							}
+							else{
+								if(cont > rec){
+									System.out.println(cont);
+									System.out.println(rec);
+									System.out.println("errooooooooooooooooooo");
+									System.exit(0);
+								}
+								
+							}
+			
+						}
+					break;
+					
+					case "imprime":
+							if(sinta.Imprime(tok)){
+								break;
+							}else{
+								System.out.println("erro na impressao!!!"); System.exit(0);
+							}
+					break;
+				}
+					
+				
+			}
+			
+		}
+	}
 		
+	/*	
 		this.linhas = l;
 		int i;
 		for(i = 0; i < linhas.length; i++){
 			if(this.linhas[i] != null && !this.linhas[i].equals(" ")){
 				String token = linhas[i].trim();
 				int ind = i;
-				if(sinta.sintaxe(token, ind)){   // na logica vai tem que voltar o verdadeiro o falso, para não executar a linha de baixo, por exemplo se der falso ate nao acar a linha que tem o fim se nao executa
-					continue;
-				}else{
+				int rec = 0;
+				//if(sinta.sintaxe(token, ind)){   // na logica vai tem que voltar o verdadeiro o falso, para não executar a linha de baixo, por exemplo se der falso ate nao acar a linha que tem o fim se nao executa
 					tok = linhas[i].trim().split(" ");
 					System.out.println(tok[0]);
-					System.out.println(linhas.length);
-					while(i < linhas.length){
-						//if(linhas[i] == null){
-						//	 i++;
-						//	 if(i + 1 == linhas.length){
-						//		 System.out.println("errooooo!!!"); System.exit(0);
-						//	 }
-						//	 continue;
-						// }
-						if(tok[0].equals("se") && linhas[i].equals("fim se")){ 	// problema no se, testar....
-							System.out.println(tok[0]);
+					System.out.println(l.length);
+					
+					for(i = 0; l[i] != null; i++){
+						 rec++;
+					}
+					i = 0;
+					while(i < l.length && l[i] != null){
+					
+						if(tok[0].equals("se") && linhas[i].contains("fim se")){ 	// problema no se, testar....
+							System.out.println("deu certo o se, encontrou o fim");
 							break;
 						}
 						else if(tok[0].equals("enquanto") && linhas[i].equals("fim enquanto")){
@@ -56,13 +110,23 @@ class Interpretador {
 						else{
 							i++;
 						}
+						
 					}
-				}
+					if(i != rec){
+						if(sinta.sintaxe(token, ind))
+						continue;
+					}
+				
+			//	}
+				//else{
+			//		continue;
+				//}
 			}
 				
 		}
 		//sinta.imprime();
+		*/
     }
     
-}
+
 
--- Sintaxe.java
+++ Sintaxe.java
@@ -7,11 +7,109 @@ class Sintaxe{
 	Logico log = new Logico();
 	Operacoes op = new Operacoes();
 	
-	public boolean sintaxe(String l, int lin){
-		//System.out.println(l);
-		String[] linhas;
+	public boolean se(String[] linhas){
+		boolean te = false;
+		int i = 0;
+		double f = -1, g = -1;
+		while(i < linhas.length){
+			if(linhas.length > 3 && linhas.length < 5){
+				if(testaVariavel(linhas[1])){   // testa se é u numero ou uma variavel...
+					f = Double.parseDouble(linhas[1]);
+				}else{
+					for(i = 0; v[i] != null; i++){
+						if(v[i].getNome().equals(linhas[1])){
+							f = v[i].getValor();
+							break;
+						}
+					}
+				}
+						
+				if(testaVariavel(linhas[3])){
+					 g = Double.parseDouble(linhas[3]);
+				}else{
+					for(i = 0; v[i] != null; i++){
+						if(v[i].getNome().equals(linhas[3])){
+							g = v[i].getValor();
+							break;
+						}
+					}
+				}	
+				}else{
+					System.out.println("erro na logica de prog..."); System.exit(0);
+				}
+				if(f != -1.0 && g != -1.0){
+					te = log.logico(linhas, f, g); //retorna false ou verdade para seguir para a proxima linha. se ele passar por todas as condiçoes retorna verdadeiro.
+					return te;
+				}else{
+					System.out.println("variavel nao declarada!!!"); System.exit(0);
+				}
+		}
+		return te;
+	}
+	
+	public boolean Imprime(String[] linhas){
+		
+		if(linhas.length < 3 && linhas[1].contains("'")){ // impressão de string
+				String r = linhas[1].substring(1, linhas[1].length() - 1); // eliminando as ''
+				System.out.println(r);
+				return true;
+		}
+		if(linhas.length > 3 && linhas[1].contains("'")){
+			String r = "";
+			for(int i = 1; i < linhas.length; i++){  // tratar erro de impressão... falta
+				r = r + linhas[i] + " ";
+			}
+			String im = r.substring(1, r.length() - 2);
+			System.out.println(im);
+			return true;
+			}
+			if(linhas.length == 2){ 
+				if(!testaVariavel(linhas[1])){
+					for(int i = 0; v[i] != null; i++){
+						if(linhas[1].equals(v[i].getNome())){
+							System.out.println(v[i].getValor());
+							return true;
+						}
+					}
+				}else if(testaVariavel(linhas[1])){
+					System.out.println(linhas[1]);
+					return true;
+				}
+			}
+			if(linhas.length > 3 && linhas.length < 5 && linhas[2].equals("+") || linhas[2].equals("-") || linhas[2].equals("*") || linhas[2].equals("/")){
+				double num = -1, num1 = -1;
+				if(testaVariavel(linhas[1])){   // testa se é um numero ou uma variavel, se nao for vai verificar nas variaveis se exite.
+					num = Double.parseDouble(linhas[1]);
+				}else{
+					for(int i = 0; v[i] != null; i++){
+						if(linhas[1].equals(v[i].getNome())){
+							num = v[i].getValor();
+						}
+					}
+				}
+				if(testaVariavel(linhas[3])){
+					num1 = Double.parseDouble(linhas[3]);
+				}else{
+					for(int i = 0; v[i] != null; i++){
+						if(linhas[3].equals(v[i].getNome())){
+							num1 = v[i].getValor(); // getValor, retorna o valor da variavel se encontrar.
+						}
+					}
+				}
+				if(num != -1 && num1 != -1 ){
+					double res = op.operacoes(linhas[2], num, num1);
+					System.out.println(res);
+					return true;
+				}	
+						
+			}
+			return false;
+					
+	}
+	
+	/*
+	public boolean sintaxe(String[] l){
 		boolean te = true;
-		linhas = l.trim().split(" ");
 			String a = linhas[0];
 			switch(a){
 			
@@ -26,7 +124,7 @@ class Sintaxe{
 									v[i].CriarVariavel(nome, rec);
 									break;
 								}
-								System.out.println("erroooo variavel '" + nome + "' foi criada duas vezes na linha " + (lin + 1)); System.exit(0);
+								System.out.println("erroooo variavel '" + nome + "' foi criada duas vezes na linha "); System.exit(0);
 							}
 						}
 					
@@ -39,7 +137,7 @@ class Sintaxe{
 									v[i].CriarVariavel(nome);
 									break;
 								}
-								System.out.println("erroooo variavel '" + nome + "' foi criada duas vezes na linha " + (lin + 1 )); System.exit(0);
+								System.out.println("erroooo variavel '" + nome + "' foi criada duas vezes na linha "); System.exit(0);
 							}
 						
 						}
@@ -49,6 +147,8 @@ class Sintaxe{
 				break;
 				
 				case "se":
+					int i = 0;
+					while(i < linhas)
 					double f = -1, g = -1;
 					if(linhas.length > 3 && linhas.length < 5){
 						if(testaVariavel(linhas[1])){   // testa se é u numero ou uma variavel...
@@ -89,7 +189,7 @@ class Sintaxe{
 					if(linhas.length < 3 && linhas[1].contains("'")){ // impressão de string
 						String r = linhas[1].substring(1, linhas[1].length() - 1); // eliminando as ''
 						System.out.println(r);
-						return true;
+						return false;
 					}
 					if(linhas.length > 3 && linhas[1].contains("'")){
 						String r = "";
@@ -98,21 +198,21 @@ class Sintaxe{
 						}
 						String im = r.substring(1, r.length() - 2);
 						System.out.println(im);
-						return true;
+						return false;
 					}
 					if(linhas.length == 2){ 
 						if(!testaVariavel(linhas[1])){
 							for(int i = 0; v[i] != null; i++){
 								if(linhas[1].equals(v[i].getNome())){
 									System.out.println(v[i].getValor());
-									return true;
+									return false;
 								}
 							}
 						}else if(testaVariavel(linhas[1])){
 							System.out.println(linhas[1]);
-							return true;
+							return false;
 						}
-						System.out.println("errooooo na impressao da variavel " + (lin + 1) + " !!!");
+						System.out.println("errooooo na impressao da variavel ");
 						System.exit(0);
 					}
 					if(linhas.length > 3 && linhas.length < 5 && linhas[2].equals("+") || linhas[2].equals("-") || linhas[2].equals("*") || linhas[2].equals("/")){
@@ -138,11 +238,11 @@ class Sintaxe{
 						if(num != -1 && num1 != -1 ){
 							double res = op.operacoes(linhas[2], num, num1);
 							System.out.println(res);
-							return true;
+							return false;
 						}	
-						return true;
+						return false;
 					}
-					System.out.println("erroooo na funcao imprime na linha " + (lin + 2) + ("!!!")); System.exit(0);
+					System.out.println("erroooo na funcao imprime na linha "); System.exit(0);
 					
 				break;
 				
@@ -155,7 +255,7 @@ class Sintaxe{
 									li = Double.parseDouble(linhas[2]);
 									v[i].setValor(li); 
 									//System.out.println(v[i].getNome() + v[i].getValor());
-									return true;
+									return false;
 								}
 							}  // falta fazer se for uma variavel...
 						}
@@ -173,6 +273,7 @@ class Sintaxe{
 			System.out.println(v[i].getNome());
 		}
 	}
+	*/
 	
 	public static boolean testaVariavel(String s){  // testa se a string que receber é um numero ou nao.
 		  try {  
@@ -185,6 +286,3 @@ class Sintaxe{
     
 }
 		
-	//for(int i = 0; i < l.length; i++) System.out.println(l[i]);
-		//if(l[4].equals(",")){
-			//String[] l1 = substring(4)// verificar
